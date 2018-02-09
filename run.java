80c2fb6f950367e80e542da15aa5760635bd8996
Nathan van der Velde <nathan@lakesnetworking.local>
2018-02-09 09:59:29 +0800

    Initial commit.
    Signed-off-by: Nathan van der Velde <nathan@lakesnetworking.local>

---
+++ run.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Nathan van der Velde
+ * Date Created: 2018-02-08
+ * Date Last Modified: 2018-02-08
+ */
+
+public class run
+{
+    public static void main(String[] args) 
+    {
+        
+    }//END main
+}//END class run
